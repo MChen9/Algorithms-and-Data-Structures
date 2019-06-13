b62b312295aa535bf312dbbb374e2b321a689e35
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-13 16:02:05 -0700

    Update 84. Largest Rectangle in Histogram.java

--- Stack/84. Largest Rectangle in Histogram.java
+++ Stack/84. Largest Rectangle in Histogram.java
@@ -2,8 +2,12 @@ class Solution {
     /**
      * [Reference](https://www.youtube.com/watch?v=ZmnqCZp9bBs)
      * Key points:
-     *   Use stack to store idx of every bar that will be used for calculating
-     *   areas;
+     *   1. Use stack to store idx of every bar that will be used for calculating
+     *      areas;
+     *   2. top is always the shorter bar, pop stack to form a rectangle till
+     *      finding a next shorter bar
+     *      2.1. idx in for loop should exceed one of length, since we need area
+     *           between idx and the top of stack
      *    
      * Time Complexity: O(n). n is the length of input, up to two pass of input   
      * Space Complexity: O(n). Stack is used.
