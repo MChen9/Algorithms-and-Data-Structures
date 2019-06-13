5fd4dccc46e6f8efcfd8fb733f8e16bfa3959da1
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-13 13:33:37 -0700

    Update 136. Single Number.java

--- Bit Manipulation/136. Single Number.java
+++ Bit Manipulation/136. Single Number.java
@@ -1,4 +1,15 @@
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/articles/single-number/)
+     * Key points:
+     *   To make sure O(1) space, we should use bit manipulation
+     *      a XOR 0 = a
+     *      a XOR a = 0
+     *      a XOR a XOR b = b
+     *
+     * Time Complexity: O(n). One pass
+     * Space Complexity: O(1).
+     */
     public int singleNumber(int[] nums) {
         int ans = 0;
         for (int num: nums) {
