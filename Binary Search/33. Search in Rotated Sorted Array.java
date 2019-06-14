cbe90187c03b9ea3e4df65e959f04200767743e7
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-14 11:22:46 -0700

    Update 33. Search in Rotated Sorted Array.java

--- Binary Search/33. Search in Rotated Sorted Array.java
+++ Binary Search/33. Search in Rotated Sorted Array.java
@@ -1,4 +1,26 @@
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14425/Concise-O(log-N)-Binary-search-solution)
+     * Key points:
+     *   1. Find the smallest number, which is the pivot of rotation
+     *      1.1. Compare mid with hi, if hi is larger, which means 
+     *           smallest element cannot in the right part, must be
+     *           mid or before
+     *      1.2. If number at mid is larger than that at hi, which means
+     *           the least number must within [mid + 1, hi]
+     *      1.3. When lo == hi or lo > hi, loop terminate since we have
+     *           found the least one
+     *   2. Find the target number by knowing where is it rotated
+     *      2.1. rotMid means the real mid of unrotated array
+     *      2.2. If nums[rotMid] > target, which means we should push
+     *           idx to the right, so we have hi = mid - 1, so that
+     *           rotMid can go back
+     *      2.3. Same as ordinary binary search, if rotMid is target,
+     *           then return
+     *          
+     * Time Complexity: O(log N). Twice binary search is also O(log N)
+     * Space Complexity: O(1).
+     */
     public int search(int[] nums, int target) {
         int lo, hi, mid, rot, rotMid;
         int n = nums.length;
