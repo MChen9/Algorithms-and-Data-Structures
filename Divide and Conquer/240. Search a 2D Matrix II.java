508c95f5388860beb0a7870bafc30ccda195b8db
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-17 11:19:52 -0700

    Update 240. Search a 2D Matrix II.java

--- Divide and Conquer/240. Search a 2D Matrix II.java
+++ Divide and Conquer/240. Search a 2D Matrix II.java
@@ -1,4 +1,18 @@
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/articles/search-a-2d-matrix-ii/)
+     * Key points:
+     *   1. Different from [74. Search a 2D Matrix], the last element of last row
+     *     is not neccessarily larger than the first element of current row;
+     *   2. Increase from left to right and from up to down, hence, start from 
+     *      bottom left, if pivot is larger than target, we move up, else, move
+     *      right.
+     *       
+     * Time Complexity: O(m + n). Because row can only be decremented mm times and 
+     * col can only be incremented nn times before causing the while loop to terminate, 
+     * the loop cannot run for more than n+mn+m iterations. 
+     * Space Complexity: 0(1).
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix.length == 0) return false;
         int row = matrix.length, col = matrix[0].length;
