5a0b64c2a71d10af0754739ddb2ef3e08fc1a83c
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-17 10:54:59 -0700

    Update 74. Search a 2D Matrix.java

--- Binary Search/74. Search a 2D Matrix.java
+++ Binary Search/74. Search a 2D Matrix.java
@@ -1,4 +1,16 @@
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/articles/search-in-2d-matrix/)
+     * Key points:
+     *   1. Since the first integer of each row is greater than the last 
+     *      integer of the previous row, we can flatten the matrix to a
+     *      length of m * n sorted array;
+     *   2. Then the pivot position can be [mid / n][mid % n], n is the 
+     *      length of row, so we can use standard binary search.
+     *
+     * Time Complexity: O(log mn).
+     * Space Complexity: O(1).
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix.length == 0) return false;
         int n = matrix[0].length;
