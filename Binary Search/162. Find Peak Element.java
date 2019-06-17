352d4b99fd049fe3e035389dddf8ddf7722a3e00
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-17 11:54:37 -0700

    Update 162. Find Peak Element.java

--- Binary Search/162. Find Peak Element.java
+++ Binary Search/162. Find Peak Element.java
@@ -1,4 +1,11 @@
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/articles/find-peak-element/)
+     * Similar to [852. Peak Index in a Mountain Array]
+     *   
+     * Time Complexity: O(log n)
+     * Space Complexity: O(1)
+     */
     public int findPeakElement(int[] nums) {
         int l = 0, r = nums.length - 1;
         int mid;
