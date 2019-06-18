8e429dbd5a3379b830be159480e1d5b7dd16e9b1
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-06-18 11:52:26 -0700

    Update 295. Find Median from Data Stream.java

--- Design/295. Find Median from Data Stream.java
+++ Design/295. Find Median from Data Stream.java
@@ -1,5 +1,18 @@
 class MedianFinder {
-    
+    /**
+     * [Reference](https://leetcode.com/problems/find-median-from-data-stream/discuss/74062/Short-simple-JavaC%2B%2BPython-O(log-n)-%2B-O(1))
+     * Key points:
+     *   1. Maintain two heaps, median = (minHeap.poll() - maxHeap.poll())/2,
+     *      1.1. maxHeap is for storing -minHeap.poll(), which means converting
+     *           smallest number to largest number, then pop the largest number
+     *           with minus sign
+     *      1.2. To make sure minHeap's length equals to maxHeap's, or one more
+     *           larger, then use the trick as below
+     *   2. Maintain two heaps so that findMedian() can take constant time
+     *
+     * Time Complexity: O(log n) for addNum(), O(1) for findMedian()
+     * Space Complexity: O(n)
+     */
     Queue<Long> maxHeap, minHeap;
     
     /** initialize your data structure here. */
@@ -12,7 +25,7 @@ class MedianFinder {
         minHeap.offer((long) num);
         maxHeap.offer(- minHeap.poll());
         
-        if (minHeap.size() < maxHeap.size())
+        if (minHeap.size() < maxHeap.size()) // make sure size are one more or equal
             minHeap.offer(- maxHeap.poll());
     }
     
