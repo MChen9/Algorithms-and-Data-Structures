f911786357e0a457150d49b0d5e1666e311f8252
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-04 16:34:07 -0700

    Update 513. Find Bottom Left Tree Value.java

--- DFS&BFS/513. Find Bottom Left Tree Value.java
+++ DFS&BFS/513. Find Bottom Left Tree Value.java
@@ -8,6 +8,14 @@
  * }
  */
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/problems/find-bottom-left-tree-value/discuss/98786/Verbose-Java-Solution-Binary-tree-level-order-traversal)
+     * Key Points:
+     *   Use level-order traversal and record the first value in every level
+     * 
+     * Time Complexity: O(N)
+     * Space Complexity: O(N)
+     */
     public int findBottomLeftValue(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
@@ -16,7 +24,7 @@ class Solution {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 TreeNode temp = queue.poll();
-                if (i == 0) val = temp.val;
+                if (i == 0) val = temp.val; // record the first value in every level
                 if (temp.left != null) queue.offer(temp.left);
                 if (temp.right != null) queue.offer(temp.right);
             }
