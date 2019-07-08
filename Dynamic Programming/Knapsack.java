2f9155f63ac65bce35f86c077320cfe28345303e
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-07 23:00:08 -0700

    Update Knapsack.java

--- Dynamic Programming/Knapsack.java
+++ Dynamic Programming/Knapsack.java
@@ -1,11 +1,24 @@
 import java.util.*;
 
 class KnapSack {
+	/**
+	 * Key Points:
+	 *   1. i means from 0 to i items, the maximun value bag can have
+	 *   2. If w = 0 or i = 0
+	 *      2.1. If w = 0, then val must be zero
+	 *      2.2. If i = 0 and w > wt[i], then it will have value
+	 *   3. Other 
+	 *      3.1. If wt[i] <= w, then we need to put wi into bag, then 
+	 *           add the remainder dp
+	 *      3.2. If wt[i] > w, then it has no difference to dp[i-1][w],
+	 *           which means it is the same without add new item
+	 */
 	public static int knapSack(int W, int[] wt, int[] val, int n) {
 		int[][] dp = new int[n][W + 1];
 
 		for (int i = 0; i < n; i++) {
 			for (int w = 0; w <= W; w++) {
+				// fill the first column and the first row
 				if (i == 0 || w == 0) {
 					if (w == 0)
 						dp[i][w] = 0;
@@ -25,19 +38,6 @@ class KnapSack {
 		}
 		System.out.println(toString(dp));
 		return dp[n - 1][W];
-	}
-
-	public static String toString(int[][] m) {
-		String s = "[ ";
-		for (int i = 0; i < m.length; i++) {
-			s += "[";
-			for (int j = 0; j < m[0].length; j++) {
-				s += m[i][j] + ",";
-			}
-			s += "]\n";
-		}
-		s += "]";
-		return s;
 	}
 
 	public static void main(String[] args) {
