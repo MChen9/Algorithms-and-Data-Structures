cbbc0ad2676d8da82cfa0501a16655b99ccd9be4
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-09 12:26:21 -0700

    Update 39. Combination Sum.java

--- Backtracking/39. Combination Sum.java
+++ Backtracking/39. Combination Sum.java
@@ -1,4 +1,26 @@
 class Solution {
+    /**
+     * [Reference](https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning))
+     * Key Points:
+     *   1. DFS
+     *      1.1. Backtracking is slightly different from DFS, similar point is that go straight to the end
+     *           then go back to parent level, but we need to track the current state so that we would not
+     *           visit it again, e.g.
+     *             candidates = [2,3,5], target = 8
+     *                   2  3  5                            2  3  5
+     *                  /                                   /
+     *                 2                                   2
+     *                /      --> go to upper level-->      /
+     *               2                                    2
+     *              /                                     /\ \
+     *             2                                     2  3 5  <-- so we need to keep track the index of start
+     *      1.2. Use remove() to backtrack
+     *   2. Memorization
+     *      2.1. We need to record every path that sum up to target on every iteration, so we use array to 
+     *           store data
+     *      2.2. If It meet the requirement, we add it to the final array of array, but need to create a new object
+     *
+     */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> list = new ArrayList<>();
@@ -12,7 +34,7 @@ class Solution {
     private void dfs(int res, List<Integer> path, List<List<Integer>> list, int[] candidates, int start) {
         if (res >= 0) {
             if (res == 0) {
-                list.add(new ArrayList<Integer>(path));
+                list.add(new ArrayList<Integer>(path)); // create new object
             } else {
                 for (int c = start; c < candidates.length; c++) {
                     path.add(candidates[c]);
@@ -20,7 +42,6 @@ class Solution {
                     path.remove(path.size() - 1);
                 }
             }
-        } 
-        
+        }   
     }
 }
