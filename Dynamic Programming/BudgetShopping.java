43938e4b3a209a721739125c3fd3081575a6a555
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-08 21:40:45 -0700

    Update BudgetShopping.java

--- Dynamic Programming/BudgetShopping.java
+++ Dynamic Programming/BudgetShopping.java
@@ -1,6 +1,24 @@
 import java.util.*;
 
 class BudgetShopping {
+	/**
+	 * Helen has n = 50 dollars to buy notebooks from m = 2 stores described by
+	 * bundleQuantities = [20, 19], bundleCosts = [24, 20], she can buy 40 books at most.
+	 *
+	 * Key Points:
+	 *   1. It differs from 0-1Knapsack, since we can choose same kind item more than once,
+	 *      e.g. we can buy 2 bundles of $20, then we pay 48 to have 40 books
+	 *   2. We can have following transfer function
+	 *      For another example, bundleQuantities = [3, 4, 5], bundleCosts = [2, 3, 4]
+	 *        dp[0] = 0, dp[1] = 0, dp[2] = 3, dp[3] = 4, dp[4] = 6
+	 *        dp[5] = max{5 + dp[1], 4 + dp[2], 3 + dp[3]}  <-- choose items from bags
+	 *        dp[6] = max{5 + dp[2], 4 + dp[3], 3 + dp[4]}
+	 *        ...
+	 *        dp[i] = max{item_j + dp[i - weight of item_j]}
+	 *
+	 * Time Complexity: O(n^2)
+	 * Space Complexity: O(n)
+	 */
 	static int budgetShopping(int n, int[] bundleQuantities, int[] bundleCosts, int m) {
 		int[] dp = new int[n+1];
 		for (int i = 1; i <= n; i++) {
@@ -13,53 +31,6 @@ class BudgetShopping {
 		}
 		return dp[n];
 	}
-	// static int budgetShopping(int n, int[] bundleQuantities, int[] bundleCosts, int m) {
-	// 	List<Integer> Qt = new ArrayList<>();
-	// 	List<Integer> Qc = new ArrayList<>();
-	// 	for (int i = 0; i < bundleCosts.length; i++) {
-	// 		int cost = n;
-	// 		while (cost >= bundleCosts[i]) {
-	// 			Qt.add(bundleQuantities[i]);
-	// 			Qc.add(bundleCosts[i]);
-	// 			cost -= bundleCosts[i];
-	// 		}
-	// 	}
-	// 	System.out.println(Qt.toString());
-	// 	int[] wt = new int[Qc.size()];
-	// 	int[] val = new int[Qt.size()];
-	// 	for (int i = 0; i < Qt.size(); i++) {
-	// 		wt[i] = Qc.get(i);
-	// 		val[i] = Qt.get(i);
-	// 	}
-
-	// 	return knapSack(n, wt, val, Qt.size());
-
-	// }
-
-	// static int knapSack(int W, int[] wt, int[] val, int n) {
-	// 	int[][] dp = new int[n][W + 1];
-
-	// 	for (int i = 0; i < n; i++) {
-	// 		for (int w = 0; w <= W; w++) {
-	// 			if (i == 0 || w == 0) {
-	// 				if (w == 0)
-	// 					dp[i][w] = 0;
-	// 				else
-	// 					dp[i][w] = (w < wt[i]) ? 0: val[i];
-	// 			} else {
-					
-	// 				if (wt[i] <= w) {
-	// 					dp[i][w] = Math.max(dp[i - 1][w], 
-	// 						val[i] + dp[i - 1][w - wt[i]]);
-	// 				} else {
-	// 					dp[i][w] = dp[i - 1][w];
-	// 				}
-	// 			}
-				
-	// 		}
-	// 	}
-	// 	return dp[n - 1][W];
-	// }
 
 	public static void main(String[] args) {
 		// int n = 50, m = 2;
