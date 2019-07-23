eb298145a3a6c2cfc6119100bc4ec8bec2613f19
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-23 16:15:26 -0700

    Update WholeMinuteDilemma.java

--- Hashtable/WholeMinuteDilemma.java
+++ Hashtable/WholeMinuteDilemma.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 class WholeMinuteDilemma {
+    /**
+     * Key points:
+     *   1. Similar to Two Sum
+     *      1.1. Before storing to hashmap, use % to have remainder,
+     *           since (key + num) % 60 == 0, so we need to substract 
+     *           both of them
+     *      1.2. There may be some duplicates, so value is used for storing
+     *           #occurrency, so the result only need to add the #occurrency
+     *
+     * Time Complexity: O(n), one pass
+     * Space Complexity: O(1), on more than 60 , so O(60)
+     */
     static int playList(int[] array) {
         Map<Integer, Integer> map = new HashMap<>();
         int res = 0;
