4bd7a85b50083226d762734670a736235d7d1143
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-23 20:56:24 -0700

    Update 829. Consecutive Numbers Sum.java

--- Math/829. Consecutive Numbers Sum.java
+++ Math/829. Consecutive Numbers Sum.java
@@ -1,9 +1,15 @@
 class Solution {
     /**
      * Key Points:
-     *   
+     *   n + (n+1) + (n+2) + ... + (n+m-1)  =>  (2n + m - 1) * m / 2 = N
+     *   =>  (2n + m - 1) * m = 2N 
+     *   Since n > 0 and m > 0, we have 2n + m - 1 >= m
+     *   =>  (2n + m - 1) * m >= m^2  => m^2 <= 2N  => m <= sqrt(2N)
+     *   Besides, 2n = 2N / m - m + 1
+     *   We have (2N / m - m + 1) must be an integer, so 
+     *   (2N / m - m + 1) % 2 == 0
      *
-     * Time Complexity: O(sqrt(n))
+     * Time Complexity: O(sqrt(n)), m <= sqrt(2N)
      * Space Complexity: O(1)
      */
     public int consecutiveNumbersSum(int N) {
