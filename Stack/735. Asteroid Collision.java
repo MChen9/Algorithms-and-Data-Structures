85790de1fbcc0f83a16dd2b1a5dcb1ee69096e68
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-24 12:09:33 -0700

    Update 735. Asteroid Collision.java

--- Stack/735. Asteroid Collision.java
+++ Stack/735. Asteroid Collision.java
@@ -1,4 +1,20 @@
 class Solution {
+    /**
+     * Key Points:
+     *   1. If there is no collision, we say it is stable, we only need to consider
+     *      the rightmost one when adding new one, which is suitable for peek() of stack;
+     *   2. If new one is positive, even though the top element of stack is negative, they 
+     *      will not collide, so we can push it to stack safely;
+     *   3. If new one is negative, we need to consider several situation
+     *      3.1. We need to compare it with the top elements and pop it until we meet negative
+     *           one or stack is empty or top one is larger than the new one
+     *      3.2. If we meet a same mass one, then we just pop it since they will disappear together
+     *      3.3. If we do not meet the same mass and the top one is negative, we just need to push
+     *           new one
+     *    
+     * Time Complexity: O(n), since we can only push or pop n times
+     * Space Complexity: O(n), for a stack
+     */
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
         for (int a: asteroids) {
