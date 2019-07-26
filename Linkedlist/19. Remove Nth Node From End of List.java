944922d53fe1c6ef7ba11cfe178f0f0137e68692
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-25 19:41:41 -0700

    Create 19. Remove Nth Node From End of List.java

---
+++ Linkedlist/19. Remove Nth Node From End of List.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p1 = dummy;
+        ListNode p2 = p1;
+        for (int i = 0; i < n; i++) {
+            p2 = p2.next;
+        }
+        while (p2.next != null) {
+            p2 = p2.next;
+            p1 = p1.next;
+        }
+        p1.next = p1.next.next;
+        return dummy.next;
+    }
+}
