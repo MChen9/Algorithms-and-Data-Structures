ee3967c3461447e890592c5d7f23281ac62c560c
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-26 14:01:25 -0700

    Update 468. Validate IP Address.java

--- String/468. Validate IP Address.java
+++ String/468. Validate IP Address.java
@@ -1,4 +1,24 @@
 class Solution {
+    /**
+     * Key Points:
+     * 1. IPv4
+     *    1.1. "//.", since "." is a special symbol like Intege.valueOf()
+     *    1.2. If "." appears at the beginning or the end, then there is no 
+     *         tokens available before or after them
+     *    1.3. We can use Integer.valueOf() to check the valid of decimal
+     *    1.4. Token between every "." can only be less than 3 digits
+     *    1.5. No leading zero and no more than one zero if the token is zero
+     *    1.6. If it has leading zero, then #digits does not equal to actual
+     *         number digits, noted that number less than 10 and 100
+     * 2. IPv6
+     *    2.1. If the character is not digit, then we need to make sure it is
+     *         within ['A', 'F'], but noted that 'd' > 'F', we need to use 
+     *         Character.toUpperCse() to make sure they are all uppercase
+     *
+     * Time Complexity: O(n), n is the number of char, since we know IPv6 must be 
+     * less than 4 digits in every token, so the inner for loop only take O(1) time
+     * Space Complexity: O(n), array to store strings
+     */
     private boolean validIPv4(String IP) {
         if (IP.charAt(0) == '.' || IP.charAt(IP.length() -1) == '.') return false;
         String[] IPs = IP.split("\\.");
