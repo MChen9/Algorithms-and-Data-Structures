86cfacefdb93d372aae22a6c55e7a8a0cb7ffba4
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-26 20:18:14 -0700

    Update 322. Coin Change.java

--- Dynamic Programming/322. Coin Change.java
+++ Dynamic Programming/322. Coin Change.java
@@ -1,4 +1,13 @@
 class Solution {
+    /**
+     * Key Points:
+     *   1. Special handling for -1, initialize dp with max value;
+     *   2. When amount is i, we select one coin from coins, then 
+     *      the result add 1 to dp[i - coin].
+     *
+     * Time Complexity: O(amount*#coins)
+     * Space Complexity: O(amount)
+     */
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1); 
