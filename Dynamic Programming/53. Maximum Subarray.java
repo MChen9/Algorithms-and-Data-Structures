2c6e6705818793dd8879cc5f02dcec58a18d98f0
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-26 21:34:54 -0700

    Update 53. Maximum Subarray.java

--- Dynamic Programming/53. Maximum Subarray.java
+++ Dynamic Programming/53. Maximum Subarray.java
@@ -1,4 +1,12 @@
 class Solution {
+    /**
+     * Key Points:
+     *   If sum is negative, then restart, use a variable to record the max value,
+     *   add value from the second to the end, inplace
+     *
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
     public int maxSubArray(int[] nums) {
         int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
