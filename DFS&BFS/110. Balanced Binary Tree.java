e24c3aa5f170b70ac9ec62bcb6ea39de6bd2b774
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-31 14:52:09 -0700

    Update 110. Balanced Binary Tree.java

--- DFS&BFS/110. Balanced Binary Tree.java
+++ DFS&BFS/110. Balanced Binary Tree.java
@@ -8,6 +8,16 @@
  * }
  */
 class Solution {
+    /**
+     * Based on [104. Maximum Depth of Binary Tree]
+     * Key Points:
+     *   If the difference between maxDepth of left and that of right is 
+     *   larger than 1, then the field isBalanced is false, althouth it 
+     *   will go through all the tree
+     *
+     * Time Complexity: O(N), N is #nodes
+     * Space Complexity: O(N)
+     */
     boolean isBalanced = true;
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
