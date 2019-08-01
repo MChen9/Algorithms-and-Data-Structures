ad5ff04ed08dad697c92d2e09d55fef41137f1ae
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-07-31 23:57:35 -0700

    Create 394. Decode String.java

---
+++ Stack/394. Decode String.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+        String res = "";
+        Stack<Integer> count = new Stack<>();
+        Stack<String> chars = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int cnt = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    cnt = cnt * 10 + (s.charAt(idx) - '0');
+                    idx++;
+                } 
+                count.push(cnt);
+            } else if (s.charAt(idx) == '[') {
+                chars.push(res);
+                idx++;
+                res = "";
+            } else if (s.charAt(idx) == ']') {
+                StringBuilder sb = new StringBuilder(chars.pop());
+                int t = count.pop();
+                for (int i = 0; i < t; i++) {
+                    sb.append(res);
+                }
+                res = sb.toString();
+                idx++;
+            } else {
+                res += s.charAt(idx);
+                idx++;
+            }
+            
+        }
+        return res;
+    }
+}
