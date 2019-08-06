9f90077c94fac053649937b27fef874504cb7c5a
MChen9 <37673773+MChen9@users.noreply.github.com>
2019-08-06 15:28:36 -0700

    Update 528. Random Pick with Weight.java

--- Binary Search/528. Random Pick with Weight.java
+++ Binary Search/528. Random Pick with Weight.java
@@ -1,5 +1,16 @@
 class Solution {
-
+    /**
+     * Key Points:
+     *   1. Choose which item depends on its weight, so it is picking a point
+     *      in [0, 1] uniformly, the intervel it falls in;
+     *   2. The interval is sum(all items weights) long, we need to culmulate
+     *      them, e.g. [1,2,3] -> [1, 3, 6]
+     *   3. Binary search: if lo == hi, then break, if target >= array[mid], 
+     *      lo = mid + 1, because we need value in the right side.
+     *
+     * Time Complexity: O(N) for preprocessing, O(logN) for pickIndex()
+     * Space Complexity: O(N)
+     */
     List<Integer> psum = new ArrayList<>();
     int tot = 0;
     Random rand = new Random();
